d23f866b78e1094c90b23eb67cb004d62cc8a603
Peter So <peterjso23@gmail.com>
2024-04-19 21:01:44 -0400

    added liked songs as playlist and new-releases

---
+++ Spotify/SpotifyResponseBuilder.js
@@ -0,0 +1,16 @@
+import { internalGetTrack } from "./routes.js";
+
+export async function buildSpotifyPlaylist(data) {
+  let response = {};
+  response.title = "Liked Tracks";
+  response.id = "";
+  response.image = "";
+  response.tracks = await Promise.all(data.map(async (t) => {
+    return await getSpotifyTrackObject(t.spotifyId)
+  }));
+  return response;
+}
+
+async function getSpotifyTrackObject(spotifyId) {
+  return await internalGetTrack(spotifyId);
+}
